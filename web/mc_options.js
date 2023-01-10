9c18cc0997b9bb3745f37dfc83c038b6f07b87c0
Barry Nix <82537651+mcbnix@users.noreply.github.com>
2023-01-10 11:29:24 -0700

    Linting fixes for mc_options.js

--- web/mc_options.js
+++ web/mc_options.js
@@ -1,33 +1,30 @@
-
 const defaultOptions = {
-    canPrint: {
-        /** @type {boolean} */
-        value: false,
-    },
+  canPrint: {
+    /** @type {boolean} */
+    value: false,
+  },
 };
 
 const McUserOptions = Object.create(null);
 
 class MCOptions {
-    constructor() {
-        throw new Error('Cannot initialize AppOptions.');
+  constructor() {
+    throw new Error("Cannot initialize AppOptions.");
+  }
+
+  static get(name) {
+    const defaultOption = defaultOptions[name];
+    const MCUserOption = McUserOptions[name];
+    if (MCUserOption !== undefined) {
+      return MCUserOption;
     }
 
-    static get(name) {
-        let defaultOption = defaultOptions[name];
-        let MCUserOption = McUserOptions[name];
-        if (MCUserOption !== undefined) {
-            return MCUserOption;
-        }
+    return defaultOption !== undefined ? defaultOption.value : undefined;
+  }
 
-        return (defaultOption !== undefined ? defaultOption.value : undefined);
-    }
-
-    static set(name, value) {
-        McUserOptions[name] = value;
-    }
+  static set(name, value) {
+    McUserOptions[name] = value;
+  }
 }
 
-export {
-    MCOptions,
-};
+export { MCOptions };
