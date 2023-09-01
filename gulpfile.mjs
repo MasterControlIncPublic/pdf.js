e779848d3e350ed2d95d3dfad1409e9ea529a575
Jerron James <jjames@mastercontrol.com>
2023-09-01 12:42:19 -0600

    cleanup

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -75,10 +75,8 @@ const COMMON_WEB_FILES = [
 const MOZCENTRAL_DIFF_FILE = "mozcentral.diff";
 
 const DIST_REPO_URL = "https://github.com/mozilla/pdfjs-dist";
-const ARTIFACTORY_RELEASE_REPO_URL =
-    "https://mastercontrol.jfrog.io/artifactory/libs-release-local/";
-const ARTIFACTORY_SNAPSHOT_REPO_URL =
-    "https://mastercontrol.jfrog.io/artifactory/libs-snapshot-local/";
+const ARTIFACTORY_RELEASE_REPO_URL = "https://mastercontrol.jfrog.io/artifactory/libs-release-local/";
+const ARTIFACTORY_SNAPSHOT_REPO_URL = "https://mastercontrol.jfrog.io/artifactory/libs-snapshot-local/";
 
 const CONFIG_FILE = "pdfjs.config";
 const config = JSON.parse(fs.readFileSync(CONFIG_FILE).toString());
